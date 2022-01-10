77d31262f35d073906eb9031b6cbf1384ab18601
Renaud Gantois <renaud.gantois@gmail.com>
2022-01-10 17:34:29 +0100

    update app.py

--- app.py
+++ app.py
@@ -4,24 +4,23 @@ import datetime
 
 import requests
 
-img = Image.open("le_wagon.png")
-st.set_page_config(page_title="Demo Day", page_icon=img)
-'''
-# Createur de recette Model front
 
-This front queries the Createur de recette [createur de recette API](https://tblablabla.fr)
+
 
 '''
+# Projet Createur de recette
+
+Createur de recette [createur de recette API](https://tblablabla.fr)
+
+'''
+
+#img = Image.open("le_wagon.png")
+#st.image(img, caption=None,width=150)
+st.image('./le_wagon.png',width=150)
 
 
-#pickup_date = st.date_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
-# pickup_time = st.time_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
-# pickup_datetime = f'{pickup_date} {pickup_time}'
-# pickup_longitude = st.number_input('pickup longitude', value=40.7614327)
-# pickup_latitude = st.number_input('pickup latitude', value=-73.9798156)
 input_text = st.text_input('input text', value="carottes, boeuf, oignons")
 #button = st.button('Run')
-output_text = st.write('output text')
 
 # enter here the address of your flask api
 url = 'https://taxifare.lewagon.ai/predict'
@@ -34,6 +33,8 @@ params = dict(input_text=input_text,
 #prediction = response.json()
 
 #pred = prediction['prediction']
+#output_text = st.write('Output Text')
+
 pred= "This is the predicted recipe using : " + input_text
 
 if st.button("Run"):
